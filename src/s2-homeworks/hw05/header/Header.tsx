a316d576efd27c3c369da903e3af30b80d6d25c0
VitaMirosh <miroshnichenko.vitaliya1992@gmail.com>
2026-02-08 14:46:03 +0300

    add style: header and sidebar

--- src/s2-homeworks/hw05/header/Header.tsx
+++ src/s2-homeworks/hw05/header/Header.tsx
@@ -22,17 +22,18 @@ export const Header: FC<PropsType> = ({handleOpen}) => {
                     ? 'Junior Plus'
                     : 'Error'
     return (
-        <>
-            <div id={'hw5-header'} className={s.header}>
-                <img
-                    src={burgerIcon}
-                    id={'hw5-burger-menu'}
-                    className={s.burgerMenuIcon}
-                    onClick={handleOpen}
-                    alt={'open menu'}
-                />
-                <h1>{pageName}</h1>
-            </div>
-        </>
+      <>
+          <div id={'hw5-header'} className={s.header}>
+              <img
+                src={burgerIcon}
+                id={'hw5-burger-menu'}
+                className={s.burgerMenuIcon}
+                onClick={handleOpen}
+                alt={'open menu'}
+              />
+              <h1>{pageName}</h1>
+          </div>
+          <div className={s.header_line}></div>
+      </>
     )
 }
