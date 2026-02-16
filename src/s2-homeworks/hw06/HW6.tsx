93fb104dd86062024b853c57cd7112b927e1526b
VitaMirosh <miroshnichenko.vitaliya1992@gmail.com>
2026-02-16 14:27:37 +0300

    fix:restore function

--- src/s2-homeworks/hw06/HW6.tsx
+++ src/s2-homeworks/hw06/HW6.tsx
@@ -17,9 +17,9 @@ const HW6 = () => {
     const save = () => {
         saveState<string>('hw6-editable-span-value', value)
     }
-    const restore = () => {
-        // делают студенты
-
+  const restore = () => {
+      const restoredValue = restoreState<string>('hw6-editable-span-value', '')
+    setValue(restoredValue)
     }
 
     return (
@@ -35,7 +35,7 @@ const HW6 = () => {
                         onChangeText={setValue}
                         spanProps={{
                             id: 'hw6-editable-span',
-                            defaultText: 'enter text...',
+                            defaultText: 'Edit text...',
                         }}
                     />
                 </div>
