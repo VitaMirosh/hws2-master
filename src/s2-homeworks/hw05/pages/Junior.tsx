cc85cc4b2a84c6f242f038b1938ce4111755cda1
VitaMirosh <miroshnichenko.vitaliya1992@gmail.com>
2026-02-20 16:09:44 +0300

    fix:onChangeCallback in SuperSelect

--- src/s2-homeworks/hw05/pages/Junior.tsx
+++ src/s2-homeworks/hw05/pages/Junior.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import HW6 from '../../hw06/HW6';
 import s from '../../../s1-main/App.module.css';
-// import HW7 from '../../hw07/HW7'
+import HW7 from '../../hw07/HW7'
 // import HW8 from '../../hw08/HW8'
 // import HW9 from '../../hw09/HW9'
 
@@ -9,7 +9,7 @@ function Junior() {
     return (
         <div id={'hw5-page-junior'} className={s.App}>
             <HW6 />
-            {/*<HW7 />*/}
+            <HW7 />
             {/*<HW8 />*/}
             {/*<HW9 />*/}
         </div>
