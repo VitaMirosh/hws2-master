1bf80318c97e7dbd59d5326c2b59a6a7ea0d2fc4
VitaMirosh <miroshnichenko.vitaliya1992@gmail.com>
2026-02-20 20:47:00 +0300

    fix: change value

--- src/s2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
+++ src/s2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
@@ -35,7 +35,7 @@ const SuperRadio: React.FC<SuperRadioPropsType> = ({
     ...restProps
 }) => {
     const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
-        // делают студенты
+      onChangeOption?.(e.currentTarget.value) // делают студенты
     }
 
     const finalRadioClassName = s.radio + (className ? ' ' + className : '')
@@ -48,6 +48,10 @@ const SuperRadio: React.FC<SuperRadioPropsType> = ({
                       id={id + '-input-' + o.id}
                       className={finalRadioClassName}
                       type={'radio'}
+                      name={name}
+                      checked={o.value === value}
+                      value={o.value}
+
                       // name, checked, value делают студенты
 
                       onChange={onChangeCallback}
