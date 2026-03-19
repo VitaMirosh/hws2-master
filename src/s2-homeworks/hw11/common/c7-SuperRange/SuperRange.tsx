e3b80897c4ff01b5f174584b60484c4048e5da73
VitaMirosh <miroshnichenko.vitaliya1992@gmail.com>
2026-03-19 19:52:47 +0300

    add style to SuperRange

--- src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -2,14 +2,17 @@ import React from 'react'
 import {Slider, SliderProps} from '@mui/material'
 
 const SuperRange: React.FC<SliderProps> = (props) => {
-    return (
-        <Slider
-            sx={{ // стили для слайдера // пишет студент
-                
-            }}
-            {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
-        />
-    )
+  return (
+    <Slider
+      sx={{ // стили для слайдера // пишет студент
+        width: '140px',
+        height: '5px',
+        color: 'rgba(0, 204, 34, 1)',
+
+      }}
+      {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
+    />
+  )
 }
 
 export default SuperRange
