a5f47cc24bbfebcd23c7d44b0ea365e4a63a8844
VitaMirosh <miroshnichenko.vitaliya1992@gmail.com>
2026-03-24 15:17:41 +0300

    fix: function change;add:change theme

--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -2,13 +2,24 @@ const initState = {
     themeId: 1,
 }
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
+export const themeReducer = (state = initState, action: actionType): typeof initState=> { // fix any
     switch (action.type) {
         // дописать
+        case 'SET_THEME_ID':{
+          return {
+              ...state,
+            themeId:action.id
+          }
+        }
 
         default:
             return state
     }
 }
+type actionType ={
+  type: 'SET_THEME_ID'
+  id: number
+}
 
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+export const changeThemeId = (id: number): actionType => ({ type: 'SET_THEME_ID' , id }) // fix any
+
